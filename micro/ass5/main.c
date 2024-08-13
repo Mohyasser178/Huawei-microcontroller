d27a11b55480d8af6a188c31f58da395819e09ed
Mohyasser178 <mohyasser178@gmail.com>
2024-08-14 00:50:47 +0300

    Add files via upload

--- micro/ass5/main.c
+++ micro/ass5/main.c
@@ -1,28 +1,81 @@
-const unsigned char segPatterns[] = {
-    0x3F,
-    0x06,
-    0x5B,
-    0x4F,
-    0x66,
-    0x6D,
-    0x7D,
-    0x07,
-    0x7F,
-    0x6F
-};
-void main() {
-    unsigned char count = 0;
+typedef unsigned char u8; typedef unsigned long int u32;
 
-    TRISB = 0x00;
-    PORTB = 0x00;
+int SSD[]=[{
 
-    while (1) {
+0b00111111,//0
 
-        PORTB = segPatterns[count];
-        count++;
-        if (count > 9) {
-            count = 0;
-  }
-Delay_ms(1000);
-    }
+0b00000110,//1
+
+0b01011011,//2
+
+0b01001111,//3
+
+0b01100110,//4
+
+0b01101101,//5
+
+0b01111101,//6
+
+0b00000111,//7
+
+Ob01111111,//8
+
+0b01101111,//9
+}
+int i;
+
+u8 cell_4,cell_3,cell_2, cell_1;
+
+u32 num;
+
+void main()
+{
+
+
+TRISD = 0x00;
+PORTD = 0x00;
+TRISA = 0;
+PORTA = 0;
+
+num 1234;
+
+cell_4= (num/1000)%10;
+cell_3= (num/100)%10;
+cell 2= (num/10)%10;
+cell_1= (num/1)%10;
+
+while(1) {
+
+PORTD=SSD[cell 4);
+PORTA.F2=1;
+PORTA.F3=0;
+PORTA.F4=0;
+PORTA.F5=0;
+delay_ms (500);
+
+PORTD=SSD[cell 3);
+PORTA. F2=0;
+PORTA.F3=1;
+PORTA.F4=0;
+PORTA.F5=0;
+delay_ms(500);
+
+PORTD=SSD [cell_2];
+PORTA.F2=0;
+PORTA.F3=0;
+PORTA.F4-1;
+PORTA.F5=0;
+delay_ms (500);
+
+PORTD=SSD[cell_1];
+
+PORTA.F2=0;
+
+PORTA.F3=0;
+
+PORTA.F4=0;
+
+PORTA.F5=1;
+
+delay_ms(500);}
 }
